5ce9737fb5b64122bb2577ad293ec4d1590413fc
Daniel Weibel <danielmweibel@gmail.com>
2017-03-19 16:32:43 +0700

    Enable signing in to Google in order to auth to Firebase
    This requires to enable the "Google" sign-in method of the Firebase
    console website for the project. Other available Firebase sign-in
    methods are "Email/Password" and "Anonymous".
    
    The user is redirected to the SignInActivity whenever the app is
    started and there is no authenticated Firebase user. On the
    SignInActivity, the user can sign in to their Google account.
    This account is then used to sign in to Firebase.
    
    Being signed-in to Firebase allows to restore the database read
    and write access to "auth != null".

--- app/src/main/java/org/latefire/deals/MainActivity.java
+++ app/src/main/java/org/latefire/deals/MainActivity.java
@@ -1,27 +1,93 @@
 package org.latefire.deals;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.latefire.deals.managers.DatabaseManager;
 import org.latefire.deals.models.Customer;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseManager mDatabaseMgr;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    private DatabaseManager mDatabaseManager;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private GoogleApiClient mGoogleApiClient;
+    private Context mActivity;
+    private String mFirebaseUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mActivity = this;
 
-        mDatabaseMgr = DatabaseManager.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        // Test if there's an authenticated Firebase user, if not, redirect the user to sign in
+        if (mFirebaseUser == null) {
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        }
+        mFirebaseUserName = mFirebaseUser.getDisplayName();
+        getSupportActionBar().setSubtitle(String.format(getString(R.string.subtitle_main_activity), mFirebaseUserName));
+
+        // Used for enabling sign out from Google account
+        mGoogleApiClient = getGoogleApiClient();
+        mDatabaseManager = DatabaseManager.getInstance();
 
         Customer customer = new Customer("danielmweibel@gmail.com", "+41798310140", "Daniel", "Weibel");
-
-        String id = mDatabaseMgr.saveCustomer(customer);
-
+        String id = mDatabaseManager.saveCustomer(customer);
         ((TextView) findViewById(R.id.tvHello)).setText(id);
+    }
+
+    private GoogleApiClient getGoogleApiClient() {
+        return new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.d(LOG_TAG, "Could not connect to Google for signing in");
+                        Toast.makeText(mActivity, "Could not connect to Google for signing in", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API)
+                .build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Sign out from Firebase and from the Google account
+            case R.id.action_sign_out:
+                mFirebaseAuth.signOut();
+                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
+                startActivity(new Intent(this, SignInActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
