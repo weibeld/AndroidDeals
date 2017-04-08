67985682ea1837645c67f5cd866172d2422fa72a
Ken <ken.itus92@gmail.com>
2017-04-08 09:36:34 +0700

    handle back button on toolbar

--- app/src/main/java/org/latefire/deals/customer/redeem/RedemptionActivity.java
+++ app/src/main/java/org/latefire/deals/customer/redeem/RedemptionActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import org.latefire.deals.R;
 import org.latefire.deals.auth.CurrentUserManager;
 import org.latefire.deals.customer.deals.CustomerDealsActivity;
@@ -37,5 +38,15 @@ public class RedemptionActivity extends AppCompatActivity {
     DatabaseManager.getInstance().redeemDeal(userId, dealId);
     MiscUtils.toastS(mActivity, "Deal redeemed");
     startActivity(new Intent(mActivity, CustomerDealsActivity.class));
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      // Respond to the action bar's Up/Home button
+      case android.R.id.home:
+        finish();
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
